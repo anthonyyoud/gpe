6942d5597ed15a6be2b378e43b0a4f8328834d8c
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2011-07-09 16:47:26 +0100

    Added licence to all non-doc files

--- src/gpe.f90
+++ src/gpe.f90
@@ -1,6 +1,17 @@
+!Copyright 2011 Anthony Youd/Newcastle University
 !
-! Copyright (c) Anthony J. Youd/Newcastle University 2011
+!   Licensed under the Apache License, Version 2.0 (the "License");
+!   you may not use this file except in compliance with the License.
+!   You may obtain a copy of the License at
 !
+!       http://www.apache.org/licenses/LICENSE-2.0
+!
+!   Unless required by applicable law or agreed to in writing, software
+!   distributed under the License is distributed on an "AS IS" BASIS,
+!   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!   See the License for the specific language governing permissions and
+!   limitations under the License.
+
 program gpe
   ! Code to solve the Gross-Pitaevskii equation in 3D.  Parallelised using MPI.
   ! See the README file for a description and usage instructions
